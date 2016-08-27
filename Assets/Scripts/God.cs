6029f2f8afd38af69a0d2ce7f4fa2bf41f8542e0
qfeys <qfeys@skynet.be>
2016-08-27 07:54:48 +0200

    Beginning of the outliner

--- Assets/Scripts/God.cs
+++ Assets/Scripts/God.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace Assets.Scripts
 {
@@ -7,8 +7,11 @@ namespace Assets.Scripts
     public class God : MonoBehaviour {
         public static God TheOne;
 
-        int Time;
-        float deltaTime;
+        int time;
+        float clock;
+        public int timeSetting;
+        Dictionary<int, float> deltaTime = new Dictionary<int, float>() { { 1, 1.0f }, { 2, 0.5f }, { 3, 0.25f }, { 4, 0.125f }, { 5, 0.0625f } };
+        public bool isPaused;
 
         internal readonly Farm farm = new Farm();
         internal readonly Quarry quarry = new Quarry();
@@ -21,13 +24,26 @@ namespace Assets.Scripts
         void Start()
         {
             if (TheOne == null) TheOne = this;
-            Time = 0;
+            time = 0;
             People.Init();
+            timeSetting = 1;
+            isPaused = true;
         }
 
         // Update is called once per frame
         void Update() {
-            People.Tick();
+            clock += Time.deltaTime;
+            if(clock >= deltaTime[timeSetting])
+            {
+                clock = 0;
+                People.Tick();
+                farm.Tick();
+                quarry.Tick();
+                road.Tick();
+                river.Tick();
+                construction.Tick();
+                military.Tick();
+            }
         }
 
         public void Console(string s)
---
+++ Assets/Scripts/Outliner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    class Outliner : MonoBehaviour
+    {
+        static public Outliner TheOne;
+
+        public void Start()
+        {
+            if (TheOne == null) TheOne = this;
+        }
+
+        public void TimeUp()
+        {
+            God.TheOne.timeSetting++;
+            if (God.TheOne.timeSetting > 5) God.TheOne.timeSetting = 5;
+            transform.Find("PanelSpeed").GetChild(0).GetComponent<Text>().text = "Speed " + God.TheOne.timeSetting;
+        }
+        public void TimeDown()
+        {
+            God.TheOne.timeSetting--;
+            if (God.TheOne.timeSetting < 1) God.TheOne.timeSetting = 1;
+            transform.Find("PanelSpeed").GetChild(0).GetComponent<Text>().text = "Speed " + God.TheOne.timeSetting;
+        }
+        public void TimePause()
+        {
+            God.TheOne.isPaused = !God.TheOne.isPaused;
+            transform.Find("PanelSpeed").GetChild(2).GetChild(0).GetComponent<Text>().fontStyle = God.TheOne.isPaused ? FontStyle.Bold : FontStyle.Normal;
+        }
+    }
+}
