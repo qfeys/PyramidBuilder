614bed2a9bb321537bb791a65c16f8c412379a1f
qfeys <qfeys@skynet.be>
2016-08-27 11:29:16 +0200

    Procedural drawing of Realm Overview

--- Assets/Scripts/RealmOverview.cs
+++ Assets/Scripts/RealmOverview.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 
-public class RealmOverview : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class RealmOverview : MonoBehaviour
+    {
+        static public RealmOverview TheOne;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        public GameObject commuityPanel;
+
+        public void Awake() { if (TheOne == null) TheOne = this; }
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Init()
+        {
+            foreach (var com in People.communityList)
+            {
+                Transform panel = Instantiate(commuityPanel).transform;
+                panel.SetParent(transform);
+                panel.GetChild(0).GetChild(0).GetComponent<Text>().text = com.ToString();
+                Debug.Log("Panel: " + com.ToString());
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
 }
