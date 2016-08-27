75e61a70befe7ba7d28c4e61bcf4f78b1525402b
qfeys <qfeys@skynet.be>
2016-08-27 16:57:30 +0200

    Classes for the map objects

---
+++ Assets/Scripts/GOclasses/QuarryGO.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuarryGO : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
