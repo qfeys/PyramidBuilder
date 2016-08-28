2b73166aa37d1bf446a352bf3da1564935fcc577
qfeys <qfeys@skynet.be>
2016-08-28 08:37:36 +0200

    color of sliders fix

--- Assets/Scripts/RealmOverview.cs
+++ Assets/Scripts/RealmOverview.cs
@@ -14,8 +14,8 @@ namespace Assets.Scripts
 
         public void Awake() { if (TheOne == null) TheOne = this; }
 
-        Color colorNorm = new Color(219, 204, 77);
-        Color colorAlert = new Color(218, 32, 32);
+        Color colorNorm = new Color(0.855f, 0.797f, 0.301f, 0.781f);         // (219, 204, 77, 200)
+        Color colorAlert = new Color(0.852f, 0.125f, 0.152f, 0.781f);         // (218, 32, 32, 200)
 
 
 
@@ -39,23 +39,20 @@ namespace Assets.Scripts
             }
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
         bool isChanging = false;
         public void TrySetPopulation(People.Community com, float value)
         {
             if (isChanging) return;
             isChanging = true;
-            People.TrySetPopulation(com, value);
             var comList = People.communityList;
+            for(int i = 0; i<comList.Count;i++)
+                transform.GetChild(i + 1).GetChild(1).GetChild(0).GetChild(0).GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>().color = colorNorm;
+            People.TrySetPopulation(com, value);
             for (int i = 0; i < comList.Count; i++)
             {
                 transform.GetChild(i+1).GetChild(1).GetChild(0).GetChild(0).GetChild(0).GetComponent<Slider>().value = People.populationDistribution[comList[i]];
                 transform.GetChild(i+1).GetChild(1).GetChild(0).GetChild(0).GetChild(1).GetComponent<Text>().text = People.PeopleAt(comList[i]).ToString("# ##0");
+
             }
             isChanging = false;
         }
@@ -63,6 +60,11 @@ namespace Assets.Scripts
         public void TrySetFood(People.Community com, float value)
         {
             People.TrySetFood(com, value);
+            transform.Find(com.ToString()).GetChild(1).GetChild(0).GetChild(1).GetChild(1).GetComponent<Text>().text = People.foodAllowance[com].ToString("0.00");
+        }
+        public void SetFoodSlider(People.Community com, float value)
+        {
+            transform.Find(com.ToString()).GetChild(1).GetChild(0).GetChild(1).GetChild(0).GetComponent<Slider>().value = value;
             transform.Find(com.ToString()).GetChild(1).GetChild(0).GetChild(1).GetChild(1).GetComponent<Text>().text = People.foodAllowance[com].ToString("0.00");
         }
 
@@ -77,6 +79,7 @@ namespace Assets.Scripts
 
         internal void LockPopBar(People.Community com)
         {
+            Debug.Log("color: " + colorAlert.ToString());
             transform.Find(com.ToString()).GetChild(1).GetChild(0).GetChild(0).GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>().color = colorAlert;
         }
     }
