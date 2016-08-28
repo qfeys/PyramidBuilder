9d283b05531308b07e3c602f82c51d872b192489
qfeys <qfeys@skynet.be>
2016-08-28 17:36:14 +0200

    Draggable class for all other windows

---
+++ Assets/Scripts/Dragable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class Dragable : MonoBehaviour, IDragHandler
+    {
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position += (Vector3)eventData.delta;
+        }
+    }
+}
