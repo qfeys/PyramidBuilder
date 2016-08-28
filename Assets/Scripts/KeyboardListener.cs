f5414a9244556da3338a590ac0c96ce88c0a1924
qfeys <qfeys@skynet.be>
2016-08-28 22:50:14 +0200

    Keyboard shortcuts

---
+++ Assets/Scripts/KeyboardListener.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class KeyboardListener : MonoBehaviour
+    {
+        public void Update()
+        {
+            if (Input.GetButtonDown("Pause"))
+                Outliner.TheOne.TimePause();
+            if (Input.GetButtonDown("Time+"))
+                Outliner.TheOne.TimeUp();
+            if (Input.GetButtonDown("Time-"))
+                Outliner.TheOne.TimeDown();
+            if (Input.GetButtonDown("Overview"))
+                Outliner.TheOne.ToggleRealmOverview();
+        }
+    }
+}
